3c86018362d26e633b4e50c7c9a52604e9d175a7
rishabgupta <rishu007gupta@gmail.com>
2023-03-28 15:30:30 +0530

    Added Packages Tut Java

---
+++ com.company/src/cwh_20_packages_In_Java.java
@@ -0,0 +1,10 @@
+//import java.util.Scanner;
+import java.util.*; //This states that we can now use all classes which are under java.util package.
+public class cwh_20_packages_In_Java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("This is my Scanner and printing value of x: " + x);
+
+    }
+}
