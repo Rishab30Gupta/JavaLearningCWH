ae1e03abfe9357fbd452c940b8eb072a23945367
rishabgupta <rishu007gupta@gmail.com>
2023-03-29 13:15:51 +0530

    Added Errors and exceptions Java

---
+++ com.company/src/cwh_29_errors_And_Exceptions.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class cwh_29_errors_And_Exceptions {
+    public static void main(String[] args) {
+        //Syntax error demo
+        //int x = 5
+        //System.out.println("Syntax error " + x);
+
+        //Logical error demo
+        //Write a program to print prime numbers between 1-10
+        /*
+        System.out.println(2);
+        for(int i=1 ; i<5 ;i++){
+            System.out.println(2*i+1);
+        }
+        */
+
+        //Runtime error demo
+        /*
+        int k;
+        Scanner sc =new Scanner(System.in);
+        k = sc.nextInt();
+        System.out.println("Integer part of 1000 divided by k is: " + 1000 / k);
+        */
+    }
+}
